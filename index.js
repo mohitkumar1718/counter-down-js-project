b66f289f4666f214fb1d6d0079b3a775673d2b42
Mohit Kumar <mohitkr7982@gmail.com>
2024-01-13 20:19:27 +0530

    logic is implemented of  project

---
+++ index.js
@@ -0,0 +1,16 @@
+const endDate="21 March 2024 10:00 Pm";
+document.querySelector(".end-date").innerText=endDate;
+const input=document.querySelectorAll("input");
+
+const clock=()=>{
+    const end=new Date(endDate);
+    const now=new Date();
+    const diff=(end-now)/1000;
+    if(diff<0)return;
+    input[0].value=Math.floor(diff/3600/24);
+    input[1].value=Math.floor((diff/3600)%24);
+    input[2].value=Math.floor((diff/60)%60);
+    input[3].value=Math.floor((diff)%60);
+    
+}
+setInterval(clock,1000);
